6bf2a483c3ffc4db18d5e654c0cfad230257dc9f
swyuliang <36142847+swyuliang@users.noreply.github.com>
2018-05-06 22:09:26 +0800

    swyuliang
    add createElement2

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -10,7 +10,7 @@ function addLoadEvent(func) {
     }
 }
 
-addLoadEvent(prepareGallery);
+
 // addLoadEvent(showPic);
 
 function prepareGallery() {
@@ -30,6 +30,7 @@ function showPic(whichpic) {
     if(!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
+
     if(placeholder.nodeName != "IMG") return false;
     placeholder.setAttribute("src",source);
     if(document.getElementById("description")) {
@@ -42,6 +43,22 @@ function showPic(whichpic) {
     return true;
 }
 
+
+function preparePlaceholder() {
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/comic.jpg");
+    placeholder.setAttribute("alt","my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(placeholder);
+    body.appendChild(description);
+}
+//addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
 
 
 
