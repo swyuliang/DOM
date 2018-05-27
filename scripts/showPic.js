69109274cf16b5a11558721c54e355c800d53417
Joe <36142847+swyuliang@users.noreply.github.com>
2018-05-27 15:00:02 +0800

    swyuliang
    add fun insertAfert()

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -19,11 +19,13 @@ function prepareGallery() {
     if(!document.getElementById("imagegallery")) return false;
     var gallery = document.getElementById("imagegallery");
     var links =gallery.getElementsByTagName("a");
-    console.log(links);
+    //console.log(links);
     for(var i = 0;i<links.length;i++ ){
         links[i].onclick = function () {
-            return showPic(this) ? false : true;
+            // return showPic(this) ? false : true;
+            return showPic(this);
         }
+        links[i].onkeypress = links[i].onclick;
     }
 }
 
@@ -47,6 +49,10 @@ function showPic(whichpic) {
 
 
 function preparePlaceholder() {
+    if(!document.createElement) return false;
+    if(!document.createTextNode) return false;
+    if(!document.getElementById) return false;
+    if(!document.getElementById("imagegallery")) return false;
     var placeholder = document.createElement("img");
     placeholder.setAttribute("id","placeholder");
     placeholder.setAttribute("src","images/comic.jpg");
@@ -55,9 +61,24 @@ function preparePlaceholder() {
     description.setAttribute("id","description");
     var desctext = document.createTextNode("Choose an image");
     description.appendChild(desctext);
-    var body = document.getElementsByTagName("body")[0];
-    body.appendChild(placeholder);
-    body.appendChild(description);
+    // var body = document.getElementsByTagName("body")[0];
+    // body.appendChild(placeholder);
+    // body.appendChild(description);
+    var gallery = document.getElementById("imagegallery");
+    // gallery.parentNode.insertBefore(placeholder,gallery);
+    // gallery.parentNode.insertBefore(description,gallery);
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+}
+
+
+function insertAfter(newElement,targetElement) {
+    var parent = targetElement.parentElement;   //  获取目标元素的父元素
+    if(parent.lastChild == targetElement){
+        parent.appendChild(newElement);
+    }else {
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
 }
 addLoadEvent(preparePlaceholder);
 addLoadEvent(prepareGallery);
