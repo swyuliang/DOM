2d837666b90ea6b2bf92debd911d302ecbaa2c1a
Joe <36142847+swyuliang@users.noreply.github.com>
2018-06-16 15:37:06 +0800

    swyuliang

--- scripts/highlightRows.js
+++ scripts/highlightRows.js
@@ -2,7 +2,7 @@ function highlightRows() {
     if(!document.getElementsByTagName) return false;
     var rows = document.getElementsByTagName("tr");
     for(var i = 0; i<rows.length; i++){
-        row[i].onmouseover = function () {
+        rows[i].onmouseover = function () {
             this.style.fontWeight = "bold";
         }
         rows[i].onmouseout = function () {
